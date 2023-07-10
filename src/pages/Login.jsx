0180ad78749bfab8f1ce9bd49a9c1ed49518c0d1
Victorio Peagno <vpn.cash@gmail.com>
2023-07-10 17:33:59 -0300

    requisito 04, valida email e senha deixando habilitado o botao apos a validacao

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    const emailCheck = email.toLowerCase().match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+    const MIN_LENGTH_LOGIN = 6;
+    const passwordCheck = password.length > MIN_LENGTH_LOGIN;
+    if (emailCheck && passwordCheck) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [email, password]);
+
   return (
     <section>
       <form>
@@ -8,17 +23,20 @@ function Login() {
           data-testid="email-input"
           type="email"
           placeholder="Digite seu email"
+          onChange={ ({ target }) => setEmail(target.value) }
         />
 
         <input
           data-testid="password-input"
           type="password"
           placeholder="Insira sua senha"
+          onChange={ ({ target }) => setPassword(target.value) }
         />
 
         <button
           data-testid="login-submit-btn"
           type="button"
+          disabled={ disabled }
         >
           Enter
         </button>
