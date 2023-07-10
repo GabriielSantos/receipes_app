aed4f17f1427d2e6ab16de0a186d7053fa65169e
Victorio Peagno <vpn.cash@gmail.com>
2023-07-10 16:20:10 -0300

    commit inicial :hammer:

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom';
+import RecipesProvider from './context/RecipesProvider';
+import Login from './pages/Login';
+import Meals from './pages/Meals';
+import Drinks from './pages/Drinks';
+import Profile from './pages/Profile';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <RecipesProvider>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/meals" component={ Meals } />
+        <Route path="/meals/:id-da-receita" />
+        <Route path="/meals/:id-da-receita/in-progress" />
+        <Route path="/drinks" component={ Drinks } />
+        <Route path="/drinks/:id-da-receita" />
+        <Route path="/drinks/:id-da-receita/in-progress" />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/done-recipes" component={ DoneRecipes } />
+        <Route path="/favorite-recipes" component={ FavoriteRecipes } />
+      </Switch>
+    </RecipesProvider>
   );
 }
 
