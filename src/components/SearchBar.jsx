31a8fa609cac0b4807759a66dd74433c81757f1c
GabriielSantos <gabrielcsantos.dev@gmail.com>
2023-07-11 18:30:30 -0300

    req 10-12

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { getSearchBar } from '../services/apiSearch';
 
 function SearchBar() {
+  const [searchInput, setSearchInput] = useState('');
+  const [searchType, setSearchType] = useState('');
+  const route = useHistory();
+
+  const handleClickSearch = () => {
+    const routeSearch = route.location.pathname;
+
+    if (searchType === 'First Letter' && searchInput.length > 1) {
+      return global.alert('Your search must have only 1 (one) character');
+    }
+    getSearchBar(searchInput, searchType, routeSearch);
+  };
+
   return (
     <div data-testid="search-content" style={ { display: 'none' } }>
       <input
@@ -8,24 +23,45 @@ function SearchBar() {
         data-testid="search-input"
         name="search"
         placeholder="Buscar Receita"
+        value={ searchInput }
+        onChange={ ({ target }) => setSearchInput(target.value) }
       />
       <br />
       <label>
-        <input type="radio" name="search" data-testid="ingredient-search-radio" />
+        <input
+          type="radio"
+          name="search"
+          data-testid="ingredient-search-radio"
+          value="Ingredient"
+          onChange={ ({ target }) => setSearchType(target.value) }
+        />
         Ingredient
       </label>
       <label>
-        <input type="radio" name="search" data-testid="name-search-radio" />
+        <input
+          type="radio"
+          name="search"
+          data-testid="name-search-radio"
+          value="Name"
+          onChange={ ({ target }) => setSearchType(target.value) }
+        />
         Name
       </label>
       <label>
-        <input type="radio" name="search" data-testid="first-letter-search-radio" />
+        <input
+          type="radio"
+          name="search"
+          data-testid="first-letter-search-radio"
+          value="First Letter"
+          onChange={ ({ target }) => setSearchType(target.value) }
+        />
         First Letter
       </label>
       <br />
       <button
         type="button"
         data-testid="exec-search-btn"
+        onClick={ handleClickSearch }
       >
         Buscar
       </button>
