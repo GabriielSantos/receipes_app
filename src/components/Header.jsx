dcedeb1f255699479b4e5b5acf00721b5f8d7d44
Gabriel de Aguiar Fiori <gabriel_afiori@hotmail.com>
2023-07-20 08:31:52 -0300

    feat:Recover

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
-import profileIcon from '../images/profileIcon.svg';
+import profileIconY from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 import SearchBar from './SearchBar';
+import logoRecipes from '../style/imgs/logoRecipesP.svg';
+import iconMeals from '../style/imgs/iconMeals.svg';
+import iconDrinks from '../style/imgs/iconDrinks.svg';
+import profileIcon from '../style/imgs/perfilIcon.svg';
+import doneIcon from '../style/imgs/doneIcon.svg';
+import favoriteIcon from '../style/imgs/favoritesIcon.svg';
+import '../style/Header.css';
 
 export default function Header() {
   const location = useLocation();
   const history = useHistory();
+
+  const getImage = () => {
+    switch (location.pathname) {
+    case '/meals':
+      return iconMeals;
+    case '/drinks':
+      return iconDrinks;
+    case '/profile':
+      return profileIcon;
+    case '/done-recipes':
+      return doneIcon;
+    case '/favorite-recipes':
+      return favoriteIcon;
+    default:
+      return '';
+    }
+  };
 
   const getTitle = () => {
     switch (location.pathname) {
@@ -44,28 +68,52 @@ export default function Header() {
   };
 
   return (
-    <div>
-      <a
-        href="/profile"
-        onClick={ handleProfileClick }
-      >
-        <img
-          alt="Ícone de Perfil"
-          src={ profileIcon }
-          data-testid="profile-top-btn"
-        />
-      </a>
-      { showSearchIcon && (
-        <button onClick={ handleSearchClick }>
+    <>
+      <div className="headerContainer">
+        <div className="yellowContainer">
           <img
-            alt="Ícone de Pesquisa"
-            src={ searchIcon }
-            data-testid="search-top-btn"
+            src={ logoRecipes }
+            alt="logo"
+            className="logoRecipesP"
           />
-        </button>
-      )}
-      <h1 data-testid="page-title">{getTitle()}</h1>
+          <h1 className="teste">
+            RECIPES
+            <span>app</span>
+          </h1>
+          <a
+            href="/profile"
+            onClick={ handleProfileClick }
+          >
+            <img
+              className={ showSearchIcon ? 'profileIcon' : 'profileIcon2' }
+              alt="Ícone de Perfil"
+              src={ profileIconY }
+              data-testid="profile-top-btn"
+            />
+          </a>
+          {showSearchIcon && (
+            <button
+              onClick={ handleSearchClick }
+              className="searchIcon"
+            >
+              <img
+                alt="Ícone de Pesquisa"
+                src={ searchIcon }
+                data-testid="search-top-btn"
+              />
+            </button>
+          )}
+        </div>
+        <div className="whiteContainer">
+          <img
+            src={ getImage() }
+            alt="Ícone da página"
+            className="iconMeals"
+          />
+          <h1 data-testid="page-title">{getTitle()}</h1>
+        </div>
+      </div>
       <SearchBar />
-    </div>
+    </>
   );
 }
