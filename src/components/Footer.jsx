40e1e68379e7a9687aa7945e686d73af09964703
Gabriel de Aguiar Fiori <gabriel_afiori@hotmail.com>
2023-07-20 08:20:28 -0300

    feat:Recover

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-import drinkIcon from '../images/drinkIcon.svg';
-import mealIcon from '../images/mealIcon.svg';
+import drinkIcon from '../style/imgs/iconDrinks.svg';
+import mealIcon from '../style/imgs/iconMeals.svg';
 import '../style/Footer.css';
 
 function Footer() {
